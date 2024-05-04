711d94ac6313eee9117540389c6942dad8d942be
monalissa <eliza.romanova597@gmail.com>
2024-05-04 16:34:16 +0300

    add pages in front

---
+++ client/src/pages/AccountUserPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserAccountDashboard from "../modules/UserAccountDashboard/components/UserAccountDashboard";
+
+const AccountUserPage = () => {
+    return (
+        <div className="page-cover">
+            <UserAccountDashboard />
+        </div>
+    );
+};
+
+export default AccountUserPage;
---
+++ client/src/pages/MembershipsPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import MembershipList from "../modules/MembershipsList/components/ListMemberships/MembershipList";
+import MembershipDashboard from "../modules/MembershipsList/components/ListMemberships/MembershipDashboard";
+
+const MembershipsPage = () => {
+    return (
+        <div className="page-cover">
+
+            <MembershipDashboard />
+
+
+        </div>
+    );
+};
+
+export default MembershipsPage;
